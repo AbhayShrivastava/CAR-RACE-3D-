84785afb2003b92d54d0b6ae89cbd6f6b0f45c0f
AbhayShrivastava <abhayshrivastava1657@gmail.com>
2018-06-28 11:50:54 +0530

    Add files via upload

---
+++ CarMovements.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovements : MonoBehaviour {
+    public float MotorForce, SteerForce, BrakeForce;
+    public WheelCollider FR, FL, RL, RR;
+   
+	// Use this for initialization
+	void Start () {
+
+    
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+        Acceleration();
+        Steering();
+        
+      
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            Application.Quit();
+        }
+
+        
+        
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RR.brakeTorque = BrakeForce;
+            RL.brakeTorque = BrakeForce;
+           
+            
+           
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            RR.brakeTorque = 0;
+            RL.brakeTorque = 0;
+        }
+        
+
+        if (Input.GetAxis("Vertical")==0)
+        {
+            RR.brakeTorque = BrakeForce;
+            RL.brakeTorque = BrakeForce;
+           
+        }
+        else 
+        {
+            RR.brakeTorque = 0;
+            RL.brakeTorque = 0;
+        }
+        }
+	
+
+
+
+    void Acceleration()
+    {
+          float v =Input.GetAxis("Vertical")*-MotorForce;
+        
+        
+        RR.motorTorque = v;
+        RL.motorTorque = v;
+       
+    }
+
+
+
+    void Steering()
+    {
+        float h = Input.GetAxis("Horizontal") * SteerForce;
+
+
+        FL.steerAngle = h;
+        FR.steerAngle = h;
+       
+    }
+	}
+
---
+++ camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+    public GameObject Car;
+     float CarX;
+     float CarY;
+     float CarZ;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        CarX = Car.transform.eulerAngles.x;
+        CarY = Car.transform.eulerAngles.y;
+        CarZ = Car.transform.eulerAngles.z;
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+	}
+}
